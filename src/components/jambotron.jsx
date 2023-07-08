a2f6bf415b875020b8c8f91f1f713240ee0622da
oussein <ouzinnbis@hotmail.it>
2023-07-08 12:59:43 +0200

    modif

--- src/components/jambotron.jsx
+++ src/components/jambotron.jsx
@@ -5,7 +5,7 @@ import Carousel from 'react-bootstrap/Carousel';
 function jombotron() {
   return (
     <Carousel className='m-3' >
-      <Carousel.Item  style={{height:"400px",backgroundImage:"url('https://png.pngtree.com/back_origin_pic/03/88/19/883f1b0ddd7c6f341e3357fd964b2bfa.jpg')",backgroundSize:"cover",backgroundPosition:"top"} }>
+      <Carousel.Item  style={{height:"400px",backgroundImage:"url('https://images3.alphacoders.com/130/1305890.jpeg')",backgroundSize:"cover",backgroundPosition:"top"} }>
         <img
           className="d-block w-100"
           src="holder.js/800x400?text=First slide&bg=373940"
@@ -28,7 +28,7 @@ function jombotron() {
           <p>Qu'attendre de votre biographe ? - Françoise Robin - Biographe.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item  style={{height:"400px",backgroundImage:"url('https://th.bing.com/th/id/OIP.ffpEjmruHin3ChUHyqRZRAHaEK?pid=ImgDet&rs=1')",backgroundSize:"cover",backgroundPosition:"top"} }>
+      <Carousel.Item  style={{height:"400px",backgroundImage:"url('https://png.pngtree.com/back_origin_pic/03/88/19/883f1b0ddd7c6f341e3357fd964b2bfa.jpg')",backgroundSize:"cover",backgroundPosition:"top"} }>
         <img
           className="d-block w-100"
           src="holder.js/800x400?text=Third slide&bg=20232a"
@@ -47,3 +47,7 @@ function jombotron() {
 }
 
 export default jombotron;
+
+
+
+
